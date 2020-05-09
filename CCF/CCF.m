6d714fd5625ebacb9c864a3d52d22544eeab75e3
Danfeng Hong <hongdanfeng1989@gmail.com>
2020-05-09 21:31:50 +0200

    initial commit

---
+++ CCF/CCF.m
@@ -0,0 +1,10 @@
+function [M,oa,pa,ua,kappa]=CCF(traindata,TrainLabel,testdata,TestLabel,nTrees)
+% function classTest=CCF(traindata,TrainLabel,nTrees)
+train = traindata';
+trLab = TrainLabel';
+test = testdata';
+teLab = TestLabel';
+CCF = genCCF(nTrees,train,trLab); %train ccf model
+classTest = predictFromCCF(CCF,test); %predict using trained model
+[M,oa,pa,ua,kappa] = confusionMatrix( teLab, classTest ); %%evaluation on test sample
+end
